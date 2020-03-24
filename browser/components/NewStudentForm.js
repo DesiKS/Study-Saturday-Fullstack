217976882eb471ed28f3a50788b30ff997fe5212
Desislava Shunturova <shunturova@gmail.com>
2020-03-24 17:06:54 -0400

    added solution to cycle 2

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react';
 
 export default class NewStudentForm extends Component {
+  //CYCLE 2 BELOW:
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: ''
+    });
+  }
+  //CYCLE 2 ABOVE
+
   render() {
+    // console.log('HERE3333', this.state);
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            // CYCLE 2 BELOW
+            onChange={this.handleChange}
+            value={this.state.firstName}
+            // CYCLE 2 ABOVE
+          />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            // CYCLE 2 BELOW
+            onChange={this.handleChange}
+            value={this.state.lastName}
+            // CYCLE 2 ABOVE
+          />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            // CYCLE 2 BELOW
+            onChange={this.handleChange}
+            value={this.state.email}
+            // CYCLE 2 ABOVE
+          />
         </label>
 
         <button type="submit">Submit New Student</button>
